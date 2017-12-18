07b4eeae3110cd82454ad8203fe5ce06cc0a787f
enggar98 <enggar@student.uinsgd.ac.id>
2017-12-18 16:32:15 +0700

    Mobile Programming

---
+++ app/src/main/java/anif/mobpro/prak/snesorandroid/MainActivity.java
@@ -0,0 +1,51 @@
+package anif.mobpro.prak.snesorandroid;
+
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        final Sensor proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+
+        if(proximitySensor == null) {
+            Log.e(TAG, "Proximity is not available.");
+            finish(); // Close app
+        }
+
+        // Create listener
+        SensorEventListener proximitySensorListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                if(sensorEvent.values[0]<proximitySensor.getMaximumRange()){
+                    getWindow().getDecorView().setBackgroundColor(Color.RED);
+                } else{
+                    getWindow().getDecorView().setBackgroundColor(Color.GREEN);
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+            }
+        };
+
+        // Register it, specifying the polling interval in
+        // microseconds
+        sensorManager.registerListener(proximitySensorListener,
+                proximitySensor, 2 * 1000 * 1000);
+    }
+}
